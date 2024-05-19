ef82f817776ef76b8c23cb964f102fff999764be
zhutongtong <15929774304@163.com>
2024-05-19 13:22:44 +0800

    调整目录结构

--- buildPage.js
+++ buildPage.js
@@ -3,23 +3,38 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// 获取当前文件所在目录
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
 function formatTitle(variableName) {
-  // 分割字符串为单词数组，转换每个单词的首字母为大写，然后再用连字符连接
   return variableName
-    .split('-') // 假设原始字符串使用连字符分隔单词
+    .split('-')
     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join('-');
 }
+
+// 创建目录
+function makePageDir(filePath) {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  makePageDir(dirname);
+  fs.mkdirSync(dirname);
+}
+
 function generateAstroFile(variableName) {
     const capitalizedVariableName = formatTitle(variableName);
+    const dirPath = path.join(__dirname, `./src/pages/${variableName}`);
+    const filePath = path.join(dirPath, `index.astro`);
+
+    makePageDir(filePath);
+
     const content = `---
-import BlogLayout from "../layouts/Blog.astro";
-import Code from "../examples/${variableName}/${variableName}_code.md";
-import Comment from "../examples/${variableName}/${variableName}_comment.md";
-import Head from "../examples/${variableName}/${variableName}_combined_comments.md";
+import BlogLayout from "../../layouts/Blog.astro";
+import Code from "../../examples/${variableName}/${variableName}_code.md";
+import Comment from "../../examples/${variableName}/${variableName}_comment.md";
+import Head from "../../examples/${variableName}/${variableName}_combined_comments.md";
 ---
 
 <BlogLayout title="Nodejs by Example: ${capitalizedVariableName}">
@@ -30,7 +45,6 @@ import Head from "../examples/${variableName}/${variableName}_combined_comments.
 </BlogLayout>
 `;
 
-    const filePath = path.join(__dirname, `./src/pages/${variableName}.astro`);
     fs.writeFileSync(filePath, content, 'utf8');
     console.log(`File ${filePath} has been generated.`);
 }
